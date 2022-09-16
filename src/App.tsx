9104aabade4923b6a7fd918e175df08a8821b98e
K4UNG <kgzinhein.my@gmail.com>
2022-09-16 20:06:22 +0630

    add router and different pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
 
-function App() {
-  return <h1>Hi</h1>
+const App: React.FC = () => {
+  return <div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/cart' element={<Cart />} />
+    </Routes>
+  </div>
 }
 
 export default App;
